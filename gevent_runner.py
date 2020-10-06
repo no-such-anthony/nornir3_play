1625b1efdbfd86b428db4cc303a389e1a834883f
no-such-anthony <38369659+no-such-anthony@users.noreply.github.com>
2020-10-06 11:09:55 +1000

    Create gevent_runner.py

---
+++ gevent_runner.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+from gevent import monkey; monkey.patch_all()
+from gevent.pool import Pool
+from nornir.core.task import AggregatedResult, Task
+from nornir.core.inventory import Host
+from typing import List
+
+
+class gevent_runner:
+
+    def __init__(self, num_workers: int = 20) -> None:
+        self.num_workers = num_workers
+
+    def run(self, task: Task, hosts: List[Host]) -> AggregatedResult:
+
+        result = AggregatedResult(task.name)
+        futures = []
+        greenlets = []
+
+        pool = Pool(self.num_workers)
+
+        for host in hosts:
+            greenlet = pool.spawn(task.copy().start, host)
+            greenlets.append(greenlet)
+        
+        pool.join()
+        futures = [greenlet.get() for greenlet in greenlets]
+
+        for future in futures:
+            worker_result = future
+            result[worker_result.host.name] = worker_result
+
+        return result
