c9d516a36d28ab802c685b9769ca16f62d163afe
no-such-anthony <38369659+no-such-anthony@users.noreply.github.com>
2020-10-06 15:38:14 +1000

    Update gevent_runner.py

--- gevent_runner.py
+++ gevent_runner.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python
 from gevent import monkey; monkey.patch_all()
 from gevent.pool import Pool
 from nornir.core.task import AggregatedResult, Task
 from nornir.core.inventory import Host
 from typing import List
+
 
 #simple gevent runner. nothing fancy just using threading from gevent instead of concurrent.futures.
 class gevent_runner:
@@ -14,20 +14,16 @@ class gevent_runner:
     def run(self, task: Task, hosts: List[Host]) -> AggregatedResult:
 
         result = AggregatedResult(task.name)
-        futures = []
         greenlets = []
 
         pool = Pool(self.num_workers)
-
         for host in hosts:
             greenlet = pool.spawn(task.copy().start, host)
-            greenlets.append(greenlet)
-        
+            greenlets.append(greenlet)        
         pool.join()
-        futures = [greenlet.get() for greenlet in greenlets]
 
-        for future in futures:
-            worker_result = future
+        for greenlet in greenlets:
+            worker_result = greenlet.get()
             result[worker_result.host.name] = worker_result
 
         return result
