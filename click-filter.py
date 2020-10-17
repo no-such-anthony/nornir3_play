f83f4842610505a9468b40a586a7792d864a0a8e
no-such-anthony <38369659+no-such-anthony@users.noreply.github.com>
2020-10-17 17:00:17 +1000

    Create click-filter.py

---
+++ click-filter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from nornir import InitNornir
+from nornir.core.filter import F
+import click
+
+
+#examples
+#python <script> host host1 host4
+#python <script> hostname host1 host4
+#python <script> groups wilma barney
+#python <script> <data_variable> fred wilma
+@click.command()
+@click.argument('filt',nargs=-1)
+def main(filt):
+    nr = InitNornir(config_file='config.yaml')
+
+    if len(filt) > 1:
+        filter_type, filter_for = filt[0], filt[1:]
+        if filter_type == 'host':
+            nr = nr.filter(filter_func=lambda h: h.name in filter_for)
+        else:
+            #only does __any but could introduce click option for __all
+            nr = eval(f'nr.filter(F({filter_type}__any=filter_for))') 
+
+    print(nr.inventory.hosts)    
+
+
+if __name__ == '__main__':
+    main()
+
+
