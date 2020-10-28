2a8dedff1a2cf4c38a478d2e228b615bafe9aa86
no-such-anthony <38369659+no-such-anthony@users.noreply.github.com>
2020-10-29 08:45:31 +1000

    Update host_dict_inventory.py

--- host_dict_inventory.py
+++ host_dict_inventory.py
@@ -1,26 +1,40 @@
 from nornir import InitNornir
+from nornir_netmiko import netmiko_send_command
 import ruamel.yaml
 
-from nornir.core.inventory import Inventory
+from nornir.core.inventory import Inventory, Hosts, Host
+from nornir.plugins.inventory.simple import _get_inventory_element
+
+
+def show_version(task):
+    cmd = 'show version | inc uptime'
+    result = task.run(task=netmiko_send_command, name='cmd', command_string=cmd)
 
 
 def getHosts():
     #whatever you need to do to return the hosts dict
     yml = ruamel.yaml.YAML(typ="safe")
-
     hosts_file = "inventory/hosts2.yaml"
     with open(hosts_file, "r") as f:
         hosts_dict = yml.load(f)
     return hosts_dict
 
 
+def buildHosts(hosts_dict):
+    hosts = Hosts()
+    for n, h in hosts_dict.items():
+        h.pop('groups', None)
+        hosts[n] = _get_inventory_element(Host, h, n, {})
+    return hosts
+
+
 #you will still need a default dictionary to load, but overwrite after
 nr = InitNornir(config_file='config.yaml',)
 print(len(nr.inventory.hosts))
 
-setattr(nr,'inventory',Inventory(hosts=getHosts(), groups={}, defaults={}))
+setattr(nr,'inventory',Inventory(hosts=buildHosts(getHosts()), groups={}, defaults={}))
 print(len(nr.inventory.hosts))
 
-for h,d in nr.inventory.hosts.items():
-    print(h)
-    print(d)
+agg_result = nr.run(task=show_version)
+for hostname, multi_result in agg_result.items():
+    print(hostname, multi_result[1].result)
