74020782920d70939a97431176f0d48a322188eb
no-such-anthony <38369659+no-such-anthony@users.noreply.github.com>
2020-11-16 21:49:06 +1000

    Create decorator-progressbar.py

---
+++ decorator-progressbar.py
@@ -0,0 +1,52 @@
+from nornir import InitNornir
+import time
+from functools import wraps
+from tqdm import tqdm
+
+# Using a decorator to generate a basic progress bar?  Crazy stuff!
+
+class PBar:
+    def __init__(self):
+        self.enabled = True
+    
+    def start(self, num_hosts):
+        self.progress = tqdm(total=num_hosts, desc="progress",)
+    
+    def __call__(self, f):
+        @wraps(f)
+        def wrap(*args, **kwargs):
+            if self.enabled:
+                pass
+            retval = f(*args, **kwargs)
+            if self.enabled:
+                tqdm.write(args[0].host.name)
+                self.progress.update()
+            return retval
+        return wrap
+
+    def finish(self):
+        self.progress.close()
+
+pbar = PBar()
+
+@pbar
+def show_version(task):
+    time.sleep(2)
+
+
+def main():
+    nr = InitNornir(config_file='dec-config.yaml')
+    # first run
+    pbar.start(len(nr.inventory.hosts))
+    agg_result = nr.run(task=show_version)
+    pbar.finish()
+    print(agg_result)
+
+    # second run
+    pbar.start(len(nr.inventory.hosts))
+    agg_result = nr.run(task=show_version)
+    pbar.finish()
+    print(agg_result)
+
+if __name__ == '__main__':
+    main()
