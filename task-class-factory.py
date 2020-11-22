0a97c394aa57f5927191f3a5ffee565f36b03049
no-such-anthony <38369659+no-such-anthony@users.noreply.github.com>
2020-11-22 10:48:09 +1000

    Update task-class-factory.py

--- task-class-factory.py
+++ task-class-factory.py
@@ -5,7 +5,7 @@ from nornir_netmiko import netmiko_send_command
 
 #how about a class function factory or is it factory function...?
 
-def Task_Factory(task, *args, **kwargs):
+def Task_Factory(task, **kwargs):
 
     cmd = kwargs.get('cmd', None)
     cmdlet = kwargs.get('cmdlet', None)
@@ -16,24 +16,29 @@ def Task_Factory(task, *args, **kwargs):
     elif task.host.platform == 'ios' and cmd == 'show':
 
         if cmdlet == 'run':
-            return Ios_Show_Run(*args, **kwargs)
+            return Ios_Show_Run(**kwargs)
         
-    return Base_Factory(*args, **kwargs)
+    return Base_Factory(**kwargs)
 
 
 class Base_Factory():
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self,**kwargs):
         self.__name__ = self.__class__.__name__
+        self.init_kwargs = kwargs
 
-        
-    def __call__(self, task, *args, **kwargs):
-        return f'Not implemented - {task.host.platform} - {task.host.name}'
+    def __call__(self, task, **kwargs):
+        cmd = self.init_kwargs.get('cmd', None)
+        cmdlet = self.init_kwargs.get('cmdlet', None)
+        return f'cmd={cmd} cmdlet={cmdlet} is not implemented for {task.host.platform} : {task.host.name}' 
 
 
 class Ios_Show_Run(Base_Factory):
 
-    def __call__(self, task, *args, **kwargs):
+    def __call__(self, task, **kwargs):
+        self.call_kwargs = kwargs
+        #print(self.init_kwargs)
+        #print(self.call_kwargs)
         cmd = 'show running-config'
         result = task.run(task=netmiko_send_command, name=cmd, command_string=cmd)
 
